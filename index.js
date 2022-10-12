912cc3a3902aeef5a6a946922b1a47de8239cc64
gerardojuarez <gerardojuarez695@gmail.com>
2022-10-12 10:39:10 -0400

    Add /commment post route

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use("/images", express.static(path.join(__dirname, "/public/images")));
 app.use('/css', express.static(path.join(__dirname, '/public/css')))
-
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/public/index.html'))
@@ -14,5 +14,14 @@ app.get('/', (req, res) => {
 app.get('/gerardo', (req, res) => {
   res.sendFile(path.join(__dirname + '/public/gerardo.html'));
 });
+
+app.post('/comment', (req, res) => {
+  const comment = req?.body?.comment ;
+  if (!comment) {
+    res.status(400).json({message: 'Provide a comment in your body.'});
+    return;
+  }
+  res.status(200).json(comment);
+})
 
 app.listen(3000);
